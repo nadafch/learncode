15c0e13f744eb19eb3f1d46dc37e05c7bec94460
hmiftachul11 <hmiftachul11@gmail.com>
2022-09-08 04:37:23 +0700

    Hero Section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Container, Row, Col, Button, Figure } from 'react-bootstrap'
+
+function HeroSection() {
+    return (
+        <Container>
+            <Row>
+                <Col className='mt-5 pt-4'>
+                <h5 className='mt-5 mb-5 pt-5 text-start text-dark'><strong>Halo, Selamat Datang</strong></h5>
+                <h1 className='mb-5 text-start '><strong>Temukan tempat belajar ngoding yang nyaman.</strong></h1>
+                <Button 
+                    className='d-grid btn-lg' 
+                    variant="primary">Mulai Belajar</Button>{' '}
+                </Col>
+
+                <Col>
+                <Figure className='mt-5 pt-5'>
+                    <Figure.Image
+                        width={400}
+                        height={400}
+                        alt="500x500"
+                        src="assets/image/Hero-Section.png"
+                    />
+                </Figure>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default HeroSection;
